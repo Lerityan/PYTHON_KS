0c71ee5811b0493f4c6be06f8a072e1f733aa880
Lerityan <iolidan.sw@gmail.com>
2022-01-22 22:34:15 +0300

    add hw4_inp without keyboard lib

---
+++ HW_4_FOR_WHILE/HW_4_EXCHANGE_INPUT_wk.py
@@ -0,0 +1,177 @@
+import sys
+import re
+from Exchange_tools import Currency
+
+
+
+def buried():
+    print("┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n┈┈┈┈┈┈┈┈┈┈┈╱▔▔▔╲\n┈┈┈┈┈┈┈┈┈┈┈▎┊╋┊▕\n┈┈┈┈┈┈┈┈┈┈┈▎┈┃┈▕\n┈┈┈┈┈┈┈┈┈┈┈▎┈┈┈▕\n▅▅▅▅▆▆▆▆▆▆▆▇▇▇▇▇")
+    print("buried")
+
+
+gbp = Currency("GBP", 0.74)
+usd = Currency("USD", 1)
+cny = Currency("CNY", 6.34)
+eur = Currency("EUR", 0.86)
+chf = Currency("CHF", 0.91)
+
+# 1
+try:
+    amount = int(input("Start task 1: input int\n"))
+    print("Ты ввел", amount, eur.name)
+    eur.to_usd_output(amount)
+#    buried()
+except ValueError:
+    print("You failed Task 1\n")
+
+2
+try:
+    amount = int(input("Start task 2: input int\n"))
+    print("Ты ввел", amount, eur.name)
+    usd.to_self_output(eur.to_usd(amount))
+    eur.to_self_output(eur.to_usd(amount))
+    chf.to_self_output(eur.to_usd(amount))
+    gbp.to_self_output(eur.to_usd(amount))
+    cny.to_self_output(eur.to_usd(amount))
+#    buried()
+except ValueError:
+    print("You failed Task 2\n")
+
+# 3.1
+try:
+    buf_inp = input("Start task 3: input int\n")
+    amount = int(buf_inp)
+    if amount >= 0:
+        print("Ты ввел", amount, eur.name)
+        usd.to_self_output(eur.to_usd(amount))
+        eur.to_self_output(eur.to_usd(amount))
+        chf.to_self_output(eur.to_usd(amount))
+        gbp.to_self_output(eur.to_usd(amount))
+        cny.to_self_output(eur.to_usd(amount))
+    else:
+        print("Введите положительное число.\n")
+except ValueError:
+    if buf_inp == "":
+        print("Вы ввели пустое поле. Введите число.\n")
+    elif re.findall(r'[a-zA-Zа-яА-ЯёЁ]', buf_inp) != []:
+        print("Вы ввели не число. Введите число.\n")
+    else:
+        print("Вы ввели какую то дичь.\n")
+
+# buried()
+
+# 4.1
+print("Start task 4")
+flag = True
+
+
+
+
+
+while flag:
+    try:
+        buf_inp1 = input("Выберите валюту из ['USD','EUR','CHF','GBP','CNY']\n")
+        buf_inp2 = input("Введите сумму\n")
+        amount = int(buf_inp2)
+        if amount >= 0:
+            print("Вы ввели сумму", amount, "и валюту", buf_inp1)
+            if str.lower(buf_inp1) == "usd":
+                usd.to_self_output(eur.to_usd(amount))
+            elif str.lower(buf_inp1) == "eur":
+                eur.to_self_output(eur.to_usd(amount))
+            elif str.lower(buf_inp1) == "chf":
+                chf.to_self_output(eur.to_usd(amount))
+            elif str.lower(buf_inp1) == "gbp":
+                gbp.to_self_output(eur.to_usd(amount))
+            elif str.lower(buf_inp1) == "cny":
+                cny.to_self_output(eur.to_usd(amount))
+            elif str.lower(buf_inp1) == "":
+                print("Но не указали название валюты\n")
+            else:
+                print("Мы не можем конвертировать эту валюту\n")
+        else:
+            print("Введите положительное число.\n")
+    except ValueError:
+        if buf_inp2 == "":
+            print("Вы ввели пустое поле. Введите число.\n")
+        elif re.findall(r'[a-zA-Zа-яА-ЯёЁ]', buf_inp2) != []:
+            print("Вы ввели не число. Введите число.\n")
+        else:
+            print("Вы ввели какую то дичь.\n")
+    except KeyboardInterrupt:
+        print()
+        flag = False
+
+# 3.2
+nf_gbp_c = 0.74
+nf_usd_c = 1
+nf_cny_c = 6.34
+nf_eur_c = 0.86
+nf_chf_c = 0.91
+
+nf_gbp_n = "GBP"
+nf_eur_n = "EUR"
+nf_usd_n = "USD"
+nf_cny_n = "CNY"
+nf_chf_n = "CHF"
+
+try:
+    buf_inp = input("Start task 3: input int\n")
+    amount = int(buf_inp)
+    if amount >= 0:
+        print("Ты ввел", amount, nf_eur_n)
+        print("Конвертированная сумма в", nf_gbp_n, "=", "%.2f" % (amount / nf_eur_c * nf_gbp_c))
+        print("Конвертированная сумма в", nf_eur_n, "=", "%.2f" % (amount / nf_eur_c * nf_eur_c))
+        print("Конвертированная сумма в", nf_usd_n, "=", "%.2f" % (amount / nf_eur_c * nf_usd_c))
+        print("Конвертированная сумма в", nf_cny_n, "=", "%.2f" % (amount / nf_eur_c * nf_cny_c))
+        print("Конвертированная сумма в", nf_chf_n, "=", "%.2f" % (amount / nf_eur_c * nf_chf_c))
+
+    else:
+        print("Введите положительное число.\n")
+except ValueError:
+    if buf_inp == "":
+        print("Вы ввели пустое поле. Введите число.\n")
+    elif re.findall(r'[a-zA-Zа-яА-ЯёЁ]', buf_inp) != []:
+        print("Вы ввели не число. Введите число.\n")
+    else:
+        print("Вы ввели какую то дичь.\n")
+# buried()
+
+# 4.2
+
+flag = True
+
+while flag:
+
+    try:
+        buf_inp1 = input("Выберите валюту из ['USD','EUR','CHF','GBP','CNY']\n")
+        buf_inp2 = input("Введите сумму\n")
+        amount = int(buf_inp2)
+        if amount >= 0:
+            print("Вы ввели сумму", amount, "и валюту", buf_inp1)
+            if str.lower(buf_inp1) == "usd":
+                print("Конвертированная сумма в", nf_usd_n, "=", "%.2f" % (amount / nf_eur_c * nf_usd_c))
+            elif str.lower(buf_inp1) == "eur":
+                print("Конвертированная сумма в", nf_eur_n, "=", "%.2f" % (amount / nf_eur_c * nf_eur_c))
+            elif str.lower(buf_inp1) == "chf":
+                print("Конвертированная сумма в", nf_chf_n, "=", "%.2f" % (amount / nf_eur_c * nf_chf_c))
+            elif str.lower(buf_inp1) == "gbp":
+                print("Конвертированная сумма в", nf_gbp_n, "=", "%.2f" % (amount / nf_eur_c * nf_gbp_c))
+            elif str.lower(buf_inp1) == "cny":
+                print("Конвертированная сумма в", nf_cny_n, "=", "%.2f" % (amount / nf_eur_c * nf_cny_c))
+            elif str.lower(buf_inp1) == "":
+                print("Но не указали название валюты\n")
+            else:
+                print("Мы не можем конвертировать эту валюту\n")
+        else:
+            print("Введите положительное число.\n")
+    except ValueError:
+        if buf_inp2 == "":
+            print("Вы ввели пустое поле. Введите число.\n")
+        elif re.findall(r'[a-zA-Zа-яА-ЯёЁ]', buf_inp2) != []:
+            print("Вы ввели не число. Введите число.\n")
+        else:
+            print("Вы ввели какую то дичь.\n")
+    except KeyboardInterrupt:
+        print()
+        flag = False
